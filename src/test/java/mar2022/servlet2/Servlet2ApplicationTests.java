adec6a44dc32679d30f2d66dd93145e4fadd0af2
Longstick0 <789456jang@naver.com>
2022-03-02 18:18:39 +0900

    initial commit

---
+++ src/test/java/mar2022/servlet2/Servlet2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mar2022.servlet2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servlet2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
